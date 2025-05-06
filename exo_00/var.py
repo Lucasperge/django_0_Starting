8ce8c160079e5a0b41efc6dde3afd186fdb6283a
lucasperge <lucasgarambois@gmail.com>
2025-05-06 12:51:04 +0000

    exercice

--- exo_00/var.py
+++ exo_00/var.py
@@ -1,21 +1,26 @@
+def var() :
+
 # ex00/var.py
 
-a = 42
-b = 3.14
-c = "Hello"
-d = True
-e = [1, 2, 3]
-f = (4, 5)
-g = {"a": 1}
-h = set([10, 20])
-i = None
+    a = 42
+    b = 3.14
+    c = "Hello"
+    d = True
+    e = [1, 2, 3]
+    f = (4, 5)
+    g = {"a": 1}
+    h = set([10, 20])
+    i = None
 
-print(a, "est de type", type(a))
-print(b, "est de type", type(b))
-print(c, "est de type", type(c))
-print(d, "est de type", type(d))
-print(e, "est de type", type(e))
-print(f, "est de type", type(f))
-print(g, "est de type", type(g))
-print(h, "est de type", type(h))
-print(i, "est de type", type(i))
+    print(a, "est de type", type(a))
+    print(b, "est de type", type(b))
+    print(c, "est de type", type(c))
+    print(d, "est de type", type(d))
+    print(e, "est de type", type(e))
+    print(f, "est de type", type(f))
+    print(g, "est de type", type(g))
+    print(h, "est de type", type(h))
+    print(i, "est de type", type(i))
+
+if __name__ == '__main__':
+    var()
