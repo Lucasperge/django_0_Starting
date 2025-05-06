57bc59673929fe2775b9a50ac42ea66e5359e670
lucasperge <lucasgarambois@gmail.com>
2025-05-06 12:51:34 +0000

    exercice

--- exo_01/numbers.py
+++ exo_01/numbers.py
@@ -1,10 +1,15 @@
+def numbers():
+
 # Ouvre le fichier numbers.txt en mode lecture
-with open("numbers.txt", "r") as fichier:
-    contenu = fichier.read()  # lit le contenu du fichier
+    with open("numbers.txt", "r") as fichier:
+        contenu = fichier.read()  # lit le contenu du fichier
 
 # Sépare les nombres là où il y a des virgules
-nombres = contenu.split(",")
+    nombres = contenu.split(",")
 
 # Affiche chaque nombre sur une ligne
-for nombre in nombres:
-    print(nombre)
+    for nombre in nombres:
+        print(nombre)
+
+if __name__ == '__main__':
+    numbers()
