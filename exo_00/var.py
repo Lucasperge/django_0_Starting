bc04d8b5f4a9787be58c7e10158c6b0c147bf560
lucasperge <lucasgarambois@gmail.com>
2025-05-06 10:15:40 +0000

    dossier

---
+++ exo_00/var.py
@@ -0,0 +1,21 @@
+# ex00/var.py
+
+a = 42
+b = 3.14
+c = "Hello"
+d = True
+e = [1, 2, 3]
+f = (4, 5)
+g = {"a": 1}
+h = set([10, 20])
+i = None
+
+print(f"{a} est de type {type(a)}")
+print(f"{b} est de type {type(b)}")
+print(f"{c} est de type {type(c)}")
+print(f"{d} est de type {type(d)}")
+print(f"{e} est de type {type(e)}")
+print(f"{f} est de type {type(f)}")
+print(f"{g} est de type {type(g)}")
+print(f"{h} est de type {type(h)}")
+print(f"{i} est de type {type(i)}")
